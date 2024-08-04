3b21433d7a6ea137b940aefb494522c309026237
Kaan Sonmez <83972809+kaansnmez@users.noreply.github.com>
2024-08-04 19:44:49 +0300

    Update main_3_streamlit.py

--- main_3_streamlit.py
+++ main_3_streamlit.py
@@ -613,15 +613,22 @@ def main():
                 try:
                   
                     app.wt1_signal=wt1_15m
+                    print("wt1_signal",wt1_15m)
                     app.wt2_signal=wt2_15m
+                    print("wt2_signal",wt2_15m)
                     app.time=stream.klines_df['close_time']
+                    print("time",stream.klines_df['close_time'])
                     app.kline_df=stream.klines_df
-                    print(stream.klines_df)
+                    print("kline_df",stream.klines_df)
                     app.pos_df=pos_df
+                    print("pos_df",pos_df)
                     app.profit=calc_profit(pos_df,stream,streaming=True)
+                    print("profit",calc_profit(pos_df,stream,streaming=True))
                     app.assets=get_future_balance_assets(client)
+                    print("assets",get_future_balance_assets(client))
                     if (rem_triger_time[0]==0) | ((rem_triger_time[0]%2==0) & (rem_triger_time_dup !=rem_triger_time[0])):
                         app.kline_history=kline(symbol='BTCUSDT',interval='1d')
+                        print("kline_history",kline(symbol='BTCUSDT',interval='1d'))
                     rem_triger_time_dup=rem_triger_time[0]
                     try:
                         rem_triger_time[0]=(pd.to_datetime(cm_futures_client.time()['serverTime'],unit='ms')+datetime.timedelta(hours=3)).minute
