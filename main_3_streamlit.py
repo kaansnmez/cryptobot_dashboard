c08c44293ee4e426c17d16189372812af4b33941
Kaan Sonmez <83972809+kaansnmez@users.noreply.github.com>
2024-08-04 04:10:52 +0300

    Update main_3_streamlit.py

--- main_3_streamlit.py
+++ main_3_streamlit.py
@@ -395,11 +395,13 @@ def long_pos_open(wt_signal,df_15m,stream,accumulation_decision):
     except:
         print("Something Wrong , Trying again.. ")
 def connect_google_sheetapi():
+    desired_order_list=["type","project_id","private_key_id","private_key","client_email","client_id","auth_uri","token_uri",
+                        "auth_provider_x509_cert_url","client_x509_cert_url","universe_domain"]
     service_dict={}
     for key,value in os.environ.items():
         if (key!='api_key') & (key!='api_secret'):
             service_dict[key]=value
-    SERVICE_ACCOUNT_FILE = service_dict
+    SERVICE_ACCOUNT_FILE = {k: service_dict[k] for k in desired_order_list}
     SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
     creds = None
     creds = service_account.Credentials.from_service_account_info(SERVICE_ACCOUNT_FILE,scopes=SCOPES)
